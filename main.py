0726af0ba6a3e65a467cfddd158c6ea151aff7ca
captkernel <karanparmar7993@gmail.com>
2024-07-05 19:53:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -29,4 +29,9 @@ if st.sidebar.button("Add New Stock"):
     add_stock()
 
 # st.sidebar.subheader("Issue Items")
-if st.sidebar.button("
+if st.sidebar.button("Issue Items to Departments"):
+    issue_items()
+
+# st.sidebar.subheader("Manage Departments")
+if st.sidebar.button("Manage Departments"):
+    manage_departments()
